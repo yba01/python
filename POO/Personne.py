077206f39e6fb81d8ba55eade43a59c50068db91
yba <byero0470@gmail.com>
2026-04-21 16:53:13 +0000

    my very first commit

--- POO/Personne.py
+++
@@ -1,8 +0,0 @@
-class Personne:
-    def __init__(self, code, nom, prenom, classe):
-        self.code = code
-        self.nom = nom
-        self.prenom = prenom
-        self.classe = classe
-        self.devoir_
-        self.exam_
